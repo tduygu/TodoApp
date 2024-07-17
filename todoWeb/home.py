6df1e4fa90032824b9fa3b9b4682f03c91a03203
Duygu Tuncay <duygutuncay@hotmail.com>
2024-07-17 15:12:26 +0300

    web ui of todo app is created.

---
+++ todoWeb/home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("A Todo App")
+st.subheader("This is a simple todo app")
+st.write("This app is to increase your <b>productivity.</b>", unsafe_allow_html=True)
+
+
+# to run this file, run the command
+# streamlit run [path and file name]
+# example:
+# streamlit run /home/duygu/PycharmProjects/pythonToDoApp/todoWeb/home.py
